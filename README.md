38b789dea01933be5db4f5d03152823271f33d5b
Faisalskp <faisalsaleem000786@gmail.com>
2021-04-29 23:25:02 +0500

    weather-api

---
+++ README.md
@@ -0,0 +1,45 @@
+# what is this?
+
+provides historical, current and forecasted weather data via light-speed APIs..
+
+# Installation 
+
+`npm i openweather-app-api --save`
+
+Then...
+
+```
+import { openweather-app-api } from 'openweather-app-api';
+
+var token = '' // add your api token 
+
+var coordinatesObj = { "lat": "", "lng": "" } //add lat and lng values 
+
+var locatonName = "" // add target name 
+
+```
+
+## How to Use
+
+* *Step 1* : Please login https://openweathermap.org/
+* *Step 2* : Get API key from "My API Keys" like "######################"
+* *Step 3* : Add lat and lng values in coordinatesObj 
+   
+   If you don't have lat and lng value the you can go the step4
+
+* *Step 4* : Add location name in locatonName as string. like "UK,USA,India,Pakistan"
+
+## Properties
+
+openweather-app-api provides historical, current and forecasted weather data via light-speed APIs..
+openweather-app-api require properties:
+
+* *token* - _String_ ("################")
+* *coordinatesObj* - _Object_ (with "lat" and "lng" values)
+* *locatonName* -_String_ ("Pakistan")
+  
+## Note
+   You can get weather details using both properties coordinatesObj and locatonName.
+   You can use only coordinatesObj for weather details with token.
+   You can use only locatonName for weather details with token.
+
---
+++ index.js
@@ -0,0 +1,63 @@
+var http = require('http');
+var token = "" // add your api token 
+var coordinatesObj = { "lat": "44", "lng": "70" } //add lat and lng values
+var locatonName = "Pakistan" // add target name 
+
+class weatherApp {
+
+  constructor(apikey) {
+    this.apikey = apikey;
+  }
+
+  findByCoordinates(properties) {
+    try {
+      var type = typeof (properties)
+      if (type === 'object' && type !== null) {
+        if (!properties.lat) {
+          throw "lat property is require"
+        }
+        if (!properties.lng) {
+          throw "lng property is require"
+        }
+        if (properties.lat && properties.lng) {
+          Coordinates(properties, this.apikey)
+        }
+      } else {
+        throw "Please pass only object"
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+
+  findByLocatonName(name) {
+    try {
+      var type = typeof (name)
+      if (type === 'string' && type !== null) {
+        findByName(name, this.apikey)
+      } else {
+        throw "Please pass only string"
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+}
+const appWeather = new weatherApp(token)
+
+async function Coordinates(obj, apikey) {
+  let data = await http.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${obj.lat}&lon=${obj.lon}&units=metric&appid=${apikey}`)
+  return data
+}
+async function findByName(name, apikey) {
+  let data = await http.get(`http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${apikey}`)
+  return data
+}
+
+appWeather.findByCoordinates(coordinatesObj)
+appWeather.findByLocatonName(locatonName)
+
+
+module.exports.weatherApp = weatherApp
+
+
